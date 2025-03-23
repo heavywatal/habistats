eed5905fcd6d88ff0acda978bfb9c9c17ad59814
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-03-23 17:03:44 +0900

    :memo: Add examples to koeppen.R

--- R/koeppen.R
+++ R/koeppen.R
@@ -3,6 +3,15 @@
 #' @format A `stars` object with 2 dimensions and 1 attribute.
 #' @source <https://koeppen-geiger.vu-wien.ac.at>
 #'
+#' @examples
+#' library(ggplot2)
+#' geo_jp = rnaturalearth::ne_countries(country = "Japan")
+#' kgc_jp = raster_kgc(geo_jp) |> print()
+#' ggplot() +
+#'   stars::geom_stars(data = kgc_jp) +
+#'   scale_fill_kgc(na.value = "transparent") +
+#'   coord_fixed() +
+#'   theme_minimal()
 #' @description
 #' [`raster_kgc()`] returns the KGC raster masked by a given polygon.
 #' @param mask sf or sfc object.
@@ -12,8 +21,30 @@ raster_kgc = function(mask = NULL) {
   if (is.null(mask)) {
     kg5m
   } else {
-    suppressMessages(sf::st_crop(kg5m, mask))
+    requireNamespace("stars", quietly = TRUE)
+    sf::st_crop(kg5m, mask)
   }
+}
+
+#' @param ... passed to [ggplot2::scale_fill_manual()].
+#' @rdname koeppen
+#' @export
+scale_color_kgc = function(...) {
+  ggplot2::scale_color_manual(
+    name = "K\u00f6ppen-Geiger Climate",
+    values = climate_colors,
+    ...
+  )
+}
+
+#' @rdname koeppen
+#' @export
+scale_fill_kgc = function(...) {
+  ggplot2::scale_fill_manual(
+    name = "K\u00f6ppen-Geiger Climate",
+    values = climate_colors,
+    ...
+  )
 }
 
 augment_raster = function(x, na.rm = FALSE) {
